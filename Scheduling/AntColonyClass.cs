c1f2166a71f7d358fa75e82715989b7cecf38bcf
Анастасия Пилецкая <ayp@tpu.ru>
2016-04-06 15:41:09 +0600

    Начинаю переписывать Муравьиный алгоритм, сделанный Катей. Переписываю, чтобы лучше понять

---
+++ Scheduling/AntColonyClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scheduling
+{
+    public class AntColonyClass
+    {
+        double feromonVelocity { get; set; }        //Скорость испарения феромона
+        double feromonWeight { get; set; }   //Весовой коэффициент для феромона
+        private double visionWeight { get; set; } //Весовой коэффициент для видимости
+        private int PassengersAll;  //Общее количество перевезенных пассажиров
+        private int timeStart = 0;  //Момент начала работы ТС
+        private int timeEnd = 10 * 60;  //ТС ездят 10 часов
+        
+                   
+    }
+}
---
+++ Scheduling/Bus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scheduling
+{
+    public class Bus
+    {
+        public int Capacity { get; set; }
+        public int PassengersInside { get; set; }
+        public int Number { get; set; }
+        public double NextTime { get; set; }
+    }
+}
